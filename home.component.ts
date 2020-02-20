b8e96b2449bce6c48ea4f8fa76c7a94be3b9f889
muniswetha <muniswetha.mokkala@campsystems.com>
2020-02-20 08:01:38 +0530

    Create home.component.ts

---
+++ home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from './admin.service';
+import { Reason } from './reason.model';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public reasons: Reason[]
+  public selectedReason = new Reason();
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit() {
+    this.resonCodeList();
+  }
+
+  resonCodeList() {
+  this.adminService.reasonCodeList().subscribe((data) => {
+      this.reasons = data;
+    }, (err) => {
+      console.log(err.message);
+    });;
+  }
+
+  public changeSuit(event) {
+    this.selectedReason = this.reasons.find(x => x.reasoncode == event.target.value);
+  }
+
+  public onsubmit() {
+    this.adminService.updateReason(this.selectedReason)
+      .subscribe((data) => {
+        this.reasons = data;
+      }, (err) => {
+        console.log(err.message);
+      });
+
+  }
+
+}
