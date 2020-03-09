cf146b12364e99625028fe2faabb35b37dfbdd0e
muniswetha <muniswetha.mokkala@campsystems.com>
2020-03-09 10:41:41 +0530

    Create vendor-holiday-maintenance.model.ts

---
+++ vendor-holiday-maintenance.model.ts
@@ -0,0 +1,9 @@
+import { VendorHolidayList } from './vendor-holiday-list.model';
+
+export class VendorHolidayMaintenance {
+    
+    id:number;
+    year:number;
+    holidayList:VendorHolidayList[];
+    
+}
